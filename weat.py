6b5e743bf3e968bbc3752aac776b613f4f281871
Erick0726 <42615885+Erick0726@users.noreply.github.com>
2024-11-01 13:49:22 -0400

    Update weat.py

--- weat.py
+++ weat.py
@@ -1,36 +1,45 @@
 import numpy as np
+import json
 from sklearn.metrics.pairwise import cosine_similarity
 from gensim.models import KeyedVectors
 
-#target 1
-A = ['male', 'man', 'boy', 'brother', 'he', 'him', 'his', 'son']
-#target 2
-B = ['female', 'woman', 'girl', 'sister', 'she', 'her', 'hers', 'daughter']
-#attribute 1
-X = ['math', 'algebra', 'geometry', 'calculus', 'equations', 'computation', 'numbers', 'addition']\
-#attribute 2
-Y = ['poetry', 'art', 'dance', 'literature', 'novel', 'symphony', 'drama', 'sculpture']
+def weat_score(file_name):
+    f = open(file_name)
+    data = json.load(f)
+    #target 1
+    A = data["targ1"]["examples"]
+    #target 2
+    B = data["targ2"]["examples"]
+    #attribute 1
+    X = data["attr1"]["examples"]
+    #attribute 2
+    Y = data["attr2"]["examples"]
+    f.close()
 
-def load_word_vectors_glove(glove_file):
-    word_v = {}
-    with open(glove_file, 'r', encoding='utf-8') as f:
-        for line in f:
-            values = line.split()
-            word = values[0]
-            vector = np.asarray(values[1:], dtype='float32')
-            word_v[word] = vector
-    return word_v
+    def load_word_vectors_glove(glove_file):
+        word_v = {}
+        with open(glove_file, 'r', encoding='utf-8') as f:
+            for line in f:
+                values = line.split()
+                word = values[0]
+                vector = np.asarray(values[1:], dtype='float32')
+                word_v[word] = vector
+        return word_v
 
-word_vectors = load_word_vectors_glove('glove.6B.50d.txt')
+    word_vectors = load_word_vectors_glove('glove.6B.50d.txt')
 
-def s(w, X, Y, vectors):
-    sim_X = np.mean([cosine_similarity(vectors[w].reshape(1,-1), vectors[x].reshape(1,-1)) for x in X])
-    sim_Y = np.mean([cosine_similarity(vectors[w].reshape(1,-1), vectors[y].reshape(1,-1)) for y in Y])
-    return sim_X - sim_Y
+    def s(w, X, Y, vectors):
+        sim_X = np.mean([cosine_similarity(vectors[w].reshape(1,-1), vectors[x].reshape(1,-1)) for x in X])
+        sim_Y = np.mean([cosine_similarity(vectors[w].reshape(1,-1), vectors[y].reshape(1,-1)) for y in Y])
+        return sim_X - sim_Y
 
-weat_score_glove = sum([s(a, X, Y, word_vectors) for a in A]) - sum ([s(b, X, Y, word_vectors) for b in B])
-print(f'WEAT score for GloVe: {weat_score_glove}')
+    weat_score_glove = sum([s(a.lower(), X, Y, word_vectors) for a in A]) - sum ([s(b.lower(), X, Y, word_vectors) for b in B])
+    print(f'WEAT score for GloVe ({data["targ1"]["category"]}/{data["targ2"]["category"]}/{data["attr1"]["category"]}/{data["attr2"]["category"]}): {weat_score_glove}')
 
-model = KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
-weat_score_word2vec = sum([s(a, X, Y, model) for a in A]) - sum ([s(b, X, Y, model) for b in B])
-print(f'WEAT score for Word2Vec: {weat_score_word2vec}')
+    model = KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
+    weat_score_word2vec = sum([s(a.lower(), X, Y, model) for a in A]) - sum ([s(b.lower(), X, Y, model) for b in B])
+    print(f'WEAT score for Word2Vec ({data["targ1"]["category"]}/{data["targ2"]["category"]}/{data["attr1"]["category"]}/{data["attr2"]["category"]}): {weat_score_word2vec}')
+
+weat_score("tests/weat6b.jsonl")
+weat_score("tests/weat7.jsonl")
+weat_score("tests/weat8.jsonl")
